055b39a94eb748dbd5d4e327b76bdb378dbc680f
22mx215 <22mx215@MCA.PSG>
2023-07-03 14:49:49 +0530

    first commit

---
+++ sum of array.py
@@ -0,0 +1,10 @@
+sum =input("Enter the value")
+#sum=0
+for i in arr:
+    sum=sum+1
+    return(sum)
+#if_name_=="_main_":
+    arr=[12,3,4,15]
+    n=len(arr)
+    ans =_sum(arr)
+    print("sum of the array is ",ans)
